69cebe82cadf25261d15c54ceeca4e35de127250
helenrodas <3030360790108@ingenieria.usac.edu.gt>
2024-01-29 19:41:35 -0600

    inicio proyecto

---
+++ DataForge/src/DataForge/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package DataForge;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Main {
+
+  
+    public static void main(String[] args) {
+        FrmPrincipal inicio = new FrmPrincipal();
+        inicio.setTitle("Data-Forge");
+        inicio.setDefaultCloseOperation(FrmPrincipal.EXIT_ON_CLOSE);
+        inicio.setLocationRelativeTo(null);
+        inicio.setVisible(true);
+        
+        
+    }
+    
+}
