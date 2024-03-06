b52099916db61d9a2b5cd347f68885581c7d1c60
helenrodas <3030360790108@ingenieria.usac.edu.gt>
2024-03-06 15:52:00 -0600

    agrego arreglos con arbol

--- DataForge/src/DataForge/arbol.java
+++ DataForge/src/DataForge/arbol.java
@@ -56,6 +56,9 @@ public class arbol {
             CTablaSimb simbolo = new CTablaSimb(raiz.hijos.get(4).etiqueta,
             raiz.hijos.get(2).etiqueta, "Variable",raiz.hijos.get(6).result);
             TablaSim.add(simbolo);
+        }else if(raiz.etiqueta == "TIPOEXPR" && raiz.hijos.size()==2){
+            raiz.result = raiz.hijos.get(0).etiqueta + raiz.hijos.get(1).etiqueta;
+            System.out.println("Si entre al if");
         }else if(raiz.etiqueta == "TIPOEXPR" && raiz.hijos.size()==1){
             if (raiz.hijos.get(0).etiqueta.substring(0,1).equals("\"")){
                 raiz.result = raiz.hijos.get(0).etiqueta;
@@ -86,6 +89,17 @@ public class arbol {
             }else if(raiz.hijos.get(0).etiqueta.equalsIgnoreCase("mod")){
                 raiz.result = String.valueOf(Double.parseDouble(raiz.hijos.get(2).result) % Double.parseDouble(raiz.hijos.get(4).result));
             }
+        }else if(raiz.etiqueta == "D_ARREGLO"){
+            System.out.println("Se encontro : "
+            + raiz.hijos.get(0).etiqueta
+            + " de tipo: " + raiz.hijos.get(2).etiqueta
+            + " con nombre : " + raiz.hijos.get(4).etiqueta + raiz.hijos.get(5).etiqueta
+            + " con contenido : " + raiz.hijos.get(8).result);
+//            Aqui voy a meter mi token a la tabla, ya las cosas concatenadas
+        }else if(raiz.etiqueta == "LISTA_DATOS" && raiz.hijos.size()==3){
+            raiz.result = raiz.hijos.get(0).result + raiz.hijos.get(1).etiqueta + raiz.hijos.get(2).result;
+        }else if(raiz.etiqueta == "LISTA_DATOS" && raiz.hijos.size()==1){
+            raiz.result = raiz.hijos.get(0).result;
         }
         
     }
